143ff71db0f81099d45501c2ea618e334dfbf993
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-28 15:17:16 +0300

    Add homework #6 solution

--- src/video.py
+++ src/video.py
@@ -3,6 +3,7 @@ import json
 import os
 import isodate
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -16,19 +17,24 @@ class Video:
 
     def __init__(self, id):
         self.id = id
-        self.url = f"https://youtu.be/{id}"
 
         self.api_key: str = os.getenv('YT_API_KEY')
+        self.title, self.iso_8601_duration, self.duration, self.view_count = None, None, None, None
+        self.like_count, self.comment_count, self.url = None, None, None
+
         youtube = Video.get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=id).execute()
-        # printj(video_response)
-        self.title = video_response['items'][0]['snippet']['title']
-        self.iso_8601_duration = video_response['items'][0]['contentDetails']['duration']
-        self.duration = isodate.parse_duration(self.iso_8601_duration)
-        self.view_count = int(video_response['items'][0]['statistics']['viewCount'])
-        self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
-        comment_count: int = int(video_response['items'][0]['statistics']['commentCount'])
+            self.title = video_response['items'][0]['snippet']['title']
+            self.iso_8601_duration = video_response['items'][0]['contentDetails']['duration']
+            self.duration = isodate.parse_duration(self.iso_8601_duration)
+            self.view_count = int(video_response['items'][0]['statistics']['viewCount'])
+            self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
+            self.comment_count: int = int(video_response['items'][0]['statistics']['commentCount'])
+            self.url = f"https://youtu.be/{id}"
+        except Exception as e:
+            print("Ошибка при получении информации о видеоролике")
 
     def __str__(self):
         return f"{self.title}"
